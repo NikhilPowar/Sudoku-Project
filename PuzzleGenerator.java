e875d349da1eb43f8fa4ce5187c74c0f17bc95f6
NikhilPowar <nikhilpowar16@gmail.com>
2016-10-22 16:44:25 +0530

    Update and rename PuzzleTest.java to PuzzleGenerator.java
    This file generates random sudoku puzzle with 20 filled fields

--- PuzzleGenerator.java
+++ PuzzleGenerator.java
@@ -53,18 +53,3 @@ class SudokuPuzzle{
 		return true;
 	}
 }
-public class PuzzleTest{
-	public static void main(String args[]){
-		int[][] puzzle = SudokuPuzzle.getPuzzle();
-		for(int i=0; i<9; i++){
-			if(i%3==0&&i!=0)
-				System.out.println("- - - - - - - - - - -");
-			for(int j=0; j<9; j++){
-				if(j%3==0&&j!=0)
-					System.out.print("| ");
-				System.out.print(puzzle[i][j] + " ");
-			}
-			System.out.print("\n");
-		}
-	}
-}
