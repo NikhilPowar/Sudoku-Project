726cd44797cbc8f6754b13a80da153141d5344fb
AkhilPowar <akhilpowar16@yahoo.com>
2016-10-22 16:58:08 +0530

    Removed SudokuPuzzle
    It now has its separate file

--- Sudoku.java
+++ Sudoku.java
@@ -4,74 +4,27 @@ import java.util.*;
 import java.awt.event.*;
 import java.awt.*;
 
-class SudokuPuzzle{
-	public static int[][] getPuzzle(){
-		int[][] puzzle = new int[9][9];
-		Random r = new Random();
-		int i, j, count=0;
-		while(count<20){
-			i=r.nextInt(9);
-			j=r.nextInt(9);
-			if(puzzle[i][j]==0){
-				puzzle[i][j]=r.nextInt(9)+1;
-				if(checkColumn(puzzle,j)&&checkRow(puzzle,i)&&checkGroup(puzzle,(int)j/3,(int)i/3)){
-					count++;
-					continue;
-				}
-				puzzle[i][j]=0;
-			}
-		}
-		return puzzle;
-	}
-	private static boolean checkColumn(int[][] puzzle,int x){
-		Set<Integer> coveredElements = new HashSet<Integer>();
-		for(int i = 0; i<9; i++){
-			if(puzzle[i][x] != 0){
-				if(!coveredElements.add(puzzle[i][x]))
-					return false;
-			}
-		}
-		return true;
-	}
-	private static boolean checkRow(int[][] puzzle,int y){
-		Set<Integer> coveredElements = new HashSet<Integer>();
-		for(int i = 0; i < 9; i++){
-			if(puzzle[y][i] != 0){
-				if(!coveredElements.add(puzzle[y][i]))
-					return false;
-			}
-		}
-		return true;
-	}
-	private static boolean checkGroup(int[][] puzzle,int x, int y){
-		Set<Integer> coveredElements = new HashSet<Integer>();
-		for(int i = 3*x; i < (3*x+3); i++){
-			for(int j = 3*y; j < (3*y+3); j++){
-				if(puzzle[j][i] != 0){
-					if(!coveredElements.add(puzzle[j][i]))
-						return false;
-				}
-			}
-		}
-		return true;
-	}
-}
-
 public class Sudoku{
 	final static int GRID_LENGTH = 9;
 	static int board[][] = new int[GRID_LENGTH][GRID_LENGTH];
 	
 	/*main funciton. Driver for the whole program.*/
 	public static void main(String[] args){
+		//Create and diplay the game frame
 		GameFrame.displayFrame();
+		
+		//Set initial values of board matrix
+		board = SudokuPuzzle.getPuzzle();
+		
 		//Prepare the text fields
 		for(int i=0; i<9; i++){
 			for(int j=0; j<9; j++){
 				if(board[i][j]!=0){
 					//set text field at (i, j) as board[i][j]
-					getTextField(i, j).setText(board[i][j]);
+					GamePanel.getTextField(j, i).setText(Integer.toString(board[i][j]));
+					
 					//Locking the text field
-					getTextField(i, j).setEnabled(false);
+					GamePanel.getTextField(j, i).setEnabled(false);
 				}
 			}
 		}
