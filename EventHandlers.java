ddedf622f2b7ad73494d95851ebd772b4e188955
AkhilPowar <akhilpowar16@yahoo.com>
2016-10-22 11:20:39 +0530

    Added call to updateBoard()

--- EventHandlers.java
+++ EventHandlers.java
@@ -2,6 +2,7 @@ package sudoku;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 
 class ButtonClickListener implements ActionListener{
 	public void actionPerformed(ActionEvent e){
@@ -9,7 +10,7 @@ class ButtonClickListener implements ActionListener{
 			System.exit(0);
 		}
 		else if(e.getActionCommand() == "help"){
-			//Insert code for message box stating rules of the game
+			MyJDialog helpDialog = new MyJDialog();
 		}
 		else if(e.getActionCommand() == "new"){
 			//Insert code for starting a new game
@@ -21,11 +22,18 @@ class TextEntryListener implements KeyListener{
 	public void keyPressed(KeyEvent e){};
 	public void keyReleased(KeyEvent e){};
 	public void keyTyped(KeyEvent e){
+		//Check whether entry was a valid digit
 		char c = e.getKeyChar();
-		if((c<'0'||c>'9') && c!=KeyEvent.VK_BACK_SPACE)
+		if((c<'1'||c>'9') && c!=KeyEvent.VK_BACK_SPACE){
 			e.consume();
-		if(((JTextField)e.getSource()).getText().length()>=1){
-			e.consume();
+			return;
 		}
+		else if(((JTextField)e.getSource()).getText().length()>=1){
+			e.consume();
+			return;
+		}
+		
+		//find co-ordinates of source cell and update the board
+		Sudoku.updateBoard(((MyJTextField)e.getSource()).getX(), ((MyJTextField)e.getSource()).getY(), c);
 	}
 }
